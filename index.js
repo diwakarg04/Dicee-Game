0bc3696160ee6ff6ff96abff23045df20fe87931
Diwakar Gupta <117665331+diwakarg04@users.noreply.github.com>
2023-04-01 10:20:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 var randomnumber1 = Math.floor(Math.random() * 6) + 1
-document.querySelector(".img1").setAttribute("src" , "images/dice" + randomnumber1 + ".png") ;
+document.querySelector(".img1").setAttribute("src" , "dice" + randomnumber1 + ".png") ;
 
 var randomnumber2 = Math.floor(Math.random() * 6) + 1
-document.querySelector(".img2").setAttribute("src" , "images/dice" + randomnumber2 + ".png") ;
+document.querySelector(".img2").setAttribute("src" , "dice" + randomnumber2 + ".png") ;
 
 if(randomnumber2 > randomnumber1){
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩" ;
